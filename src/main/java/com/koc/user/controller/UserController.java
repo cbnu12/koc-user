50d190bf125dd97ea592049295c644b5dbeeab49
seongjo24 <kangsj24@naver.com>
2023-06-06 20:01:38 +0900

    수정

--- src/main/java/com/koc/user/controller/UserController.java
+++ src/main/java/com/koc/user/controller/UserController.java
@@ -6,7 +6,7 @@ import com.koc.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("user")
+@RequestMapping("/user")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
