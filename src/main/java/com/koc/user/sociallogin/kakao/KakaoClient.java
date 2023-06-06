115a740b64e67b5b1b624c399a0258804aac669c
seongjo24 <kangsj24@naver.com>
2023-06-06 17:01:21 +0900

    카카오 간편로그인 API 사용

---
+++ src/main/java/com/koc/user/sociallogin/kakao/KakaoClient.java
@@ -0,0 +1,24 @@
+package com.koc.user.sociallogin.kakao;
+
+import com.koc.user.config.KakaoFeignConfiguration;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.URI;
+
+@FeignClient(name = "kakaoClient", configuration = KakaoFeignConfiguration.class)
+public interface KakaoClient {
+
+    @PostMapping(headers = {"Content-Type=application/x-www-form-urlencoded;charset=utf-8"})
+    KakaoToken getToken(URI baseUrl, @RequestParam("client_id") String restApiKey,
+                        @RequestParam("redirect_uri") String redirectUrl,
+                        @RequestParam("code") String code,
+                        @RequestParam("grant_type") String grantType);
+
+    @GetMapping
+    KakaoUserInfo getKakaoUserInfo(URI baseUrl, @RequestHeader("Authorization") String accessToken);
+
+}
